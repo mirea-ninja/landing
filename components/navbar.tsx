12fd56ced4435f2b95d464d767a1d5aaa9b2a828
Valery Verkhoturov <verkhoturov.valery@icloud.com>
2023-01-25 17:05:40 +0300

    Add fill current color of Github icon

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,10 +1,11 @@
 import Link from "next/link";
-import Image from "next/image";
 import React from "react";
 import {MoonIcon, SunIcon} from "@heroicons/react/24/outline";
 import {useTheme} from "@/context/theme-context";
 import {lightTheme, darkTheme} from "@/context/theme-context";
+import GithubMark from "@/components/github-mark";
 
+const iconSize = "w-7 h-7";
 export default function Navbar() {
 
     const {toggleTheme} = useTheme();
@@ -12,19 +13,18 @@ export default function Navbar() {
     return (
         <div className="navbar bg-base-200 shadow-md rounded-md">
             <div className="flex-1">
-                <Link href="/" className="btn btn-ghost hower:btn-gost normal-case text-xl">Mirea Ninja</Link>
+                <Link href="/" className="btn btn-ghost normal-case text-xl">Mirea Ninja</Link>
             </div>
             <div className="flex-none">
-                <label className="btn btn-square btn-ghost hower:btn-gost swap swap-rotate">
+                <label className="btn btn-square btn-ghost swap swap-rotate">
                     <input type="checkbox"
                            onChange={(event) => toggleTheme(event.currentTarget.checked ? darkTheme : lightTheme)}/>
-                    <SunIcon className="swap-off fill-current w-7 h-7"/>
-                    <MoonIcon className="swap-on fill-current w-7 h-7"/>
+                    <SunIcon className={`swap-off fill-current ${iconSize}`}/>
+                    <MoonIcon className={`swap-on fill-current ${iconSize}`}/>
                 </label>
                 <Link href="https://github.com/mirea-ninja"
-                      className="btn btn-square btn-ghost hower:btn-gost">
-                    <Image className="fill-currentColor" src="/github-mark.svg" width={24} height={24}
-                           alt="GitHub Logo"/>
+                      className="btn btn-square btn-ghost">
+                    <GithubMark className={iconSize}/>
                 </Link>
             </div>
         </div>
