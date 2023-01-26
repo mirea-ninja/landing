1fd56fab30ce1f45581e44f2ed5f69fd28c3d7f2
Dragonprod <artyom2002-98@mail.ru>
2023-01-26 11:06:42 +0000

    Deploying to gh-pages from @ mirea-ninja/landing@f58e6654a58fd13110b9fe75238dbac2551c6e48 🚀

--- _next/static/PDfBkk9-ZM_RhVjDdeiri/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/247-424ac85912b6021e.js","static/chunks/pages/index-a9a58388be6ebc22.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
