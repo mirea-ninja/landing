a259335c271d6691be1cfee302c900dc6fb19482
Dragonprod <artyom2002-98@mail.ru>
2023-01-26 14:05:33 +0300

    fix: assets path

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,27 +8,27 @@ const projects: ProjectGridProps[] = [
             title: "Mirea Ninja Bot",
             description: "Бот для беседы сообщества в Телеграме. Помогает следить за порядком в беседе. Имеется система репутации и полноценная интеграция аккаунтов в Telegram с аккаунтами на форуме",
             link: "https://t.me/mireaninja_bot",
-            icon: "/ninja_bot.jpg"
+            icon: "static/ninja_bot.jpg"
         }, {
             title: "Mirea Ninja Executor Bot",
             description: "Бот для создания, сохранение и исполнение Python кода прямо в чате. Имеется поддержка библиотек, в том числе сетевых. Выполняется в изолированном окружении",
             link: "https://t.me/mireaninja_executor_bot",
-            icon: "/executor_bot.jpg"
+            icon: "static/executor_bot.jpg"
         }, {
             title: "Расписание преподавателей МИРЭА",
             description: "Бот для просмотра расписания преподавателей МИРЭА по ФИО или расписание группы. Работает с расписанием сессии",
             link: "https://t.me/teacherschedulertu_bot",
-            icon: "/schedule_bot.jpg"
+            icon: "static/schedule_bot.jpg"
         }, {
             title: "Поиск студентов РТУ МИРЭА",
             description: "Бот для поиска группы студента по ФИО или поиска ФИО по группе. Выводит email, код студента, ссылку на профиль в ВКонтакте",
             link: "https://t.me/mirea_ninja_search_students_bot",
-            icon: "/search-students-logo.png"
+            icon: "static/search-students-logo.png"
         }, {
             title: "Траектория обучения РТУ МИРЭА",
             description: "Бот позволяет получать траекторию обучения из по коду направления, названию профиля или названию направления. Выводи по семестрам и годам поступления",
             link: "https://t.me/learning_roadmap_bot",
-            icon: "/trajectory.jpg"
+            icon: "static/trajectory.jpg"
         }]
     }, {
         title: "Веб-сайты и сервисы",
@@ -36,7 +36,7 @@ const projects: ProjectGridProps[] = [
             title: "Форум Mirea Ninja",
             description: "Наше сообщество. Здесь можно задавать вопросы, обсуждать и делиться своими знаниями",
             link: "https://mirea.ninja",
-            icon: "/forum.svg"
+            icon: "static/forum.svg"
         }, {
             title: "API Расписания МИРЭА",
             description: "API для получения расписания группы, поиска расписания преподавателей",
@@ -59,7 +59,7 @@ const projects: ProjectGridProps[] = [
             title: "Ninja Mirea App",
             description: "Приложение для просмотра расписания группы, просмотра новостей, карты. Имеется интеграция с профилем студента",
             link: "https://play.google.com/store/apps/details?id=ru.mirea.ninja",
-            icon: "/mobile.png"
+            icon: "static/mobile.png"
         }]
     }, {
         title: "Голосовые помощники",
