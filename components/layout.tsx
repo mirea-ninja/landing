7da139e680a74a47bfe5872745700ebabf120839
Dragonprod <artyom2002-98@mail.ru>
2023-01-26 14:09:56 +0300

    fix: path for logo and icon

--- components/layout.tsx
+++ components/layout.tsx
@@ -12,7 +12,7 @@ export default function Layout(props: LayoutProps) {
                 <title>Mirea Ninja</title>
                 <meta name="description" content="Наши проекты и идеи"/>
                 <meta name="viewport" content="width=device-width, initial-scale=1"/>
-                <link rel="icon" href="/favicon.ico"/>
+                <link rel="icon" href="statics/favicon.ico"/>
             </Head>
             <main className="mx-auto px-4 sm:px-6 lg:px-8 py-8 max-w-7xl">
                 <Navbar/>
