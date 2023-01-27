b1afef9007471b6c0fb780267370e895e28646d9
Valery Verkhoturov <verkhoturov.valery@icloud.com>
2023-01-27 09:58:52 +0300

    Add constants

---
+++ constants/constants.ts
@@ -0,0 +1,5 @@
+export const darkTheme = "night";
+export const lightTheme = "corporate";
+export const CMSProjectGroupsUrl = "https://cms.mirea.ninja/api/landing-project-groups?populate[projectCards][populate]=*";
+export const VKLink = "https://vk.com/mirea.ninja";
+export const GitHubLink = "https://github.com/mirea-ninja";
