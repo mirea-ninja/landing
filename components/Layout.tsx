755a0c040c8fa2cc243db943d2f6d35602b973eb
Valery Verkhoturov <verkhoturov.valery@icloud.com>
2023-01-31 19:35:38 +0300

    Add meta tags

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -13,6 +13,10 @@ export default function Layout(props: LayoutProps) {
                 <title>Mirea Ninja</title>
                 <meta name="description" content="Наши проекты и идеи"/>
                 <meta name="viewport" content="width=device-width, initial-scale=1"/>
+                <meta property="og:title" content="Mirea Ninja"/>
+                <meta property="og:site_name" content="Mirea Ninja: Navigator"/>
+                <meta property="og:description" content="Проекты для студентов РТУ МИРЭА"/>
+                <meta property="og:image" content="https://cdn.cms.mirea.ninja/navigator_logo_3527455f8d.png"/>
                 <link rel="icon" href="/static/favicon.ico"/>
             </Head>
             <main className="mx-auto px-4 sm:px-6 lg:px-8 py-4 max-w-7xl">
