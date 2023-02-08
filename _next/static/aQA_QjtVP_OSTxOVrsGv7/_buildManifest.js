5122e9a2eb21dff569b58b112ff9949dd846e1bb
0niel <51058739+0niel@users.noreply.github.com>
2023-02-08 22:17:03 +0000

    Deploying to gh-pages from @ mirea-ninja/landing@66f4cbcf4be065c53f251bb1f4795297b102fd02 🚀

--- _next/static/aQA_QjtVP_OSTxOVrsGv7/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/313-b2b0b15f76ba637e.js","static/chunks/pages/index-43871ed495adb1d4.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
