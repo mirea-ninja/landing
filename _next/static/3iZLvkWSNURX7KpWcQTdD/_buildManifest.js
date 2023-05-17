10c8a2b09fbb13ca6823cadd00b84b9c97cddd50
ValeryVerkhoturov <70258211+ValeryVerkhoturov@users.noreply.github.com>
2023-05-17 20:03:22 +0000

    Deploying to gh-pages from @ mirea-ninja/landing@8d9d55f5149d8989cd74f2567530aedd7f43714d 🚀

---
+++ _next/static/3iZLvkWSNURX7KpWcQTdD/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/313-8279acc6070c6fc7.js","static/chunks/pages/index-c5df460cb4f51e5d.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
